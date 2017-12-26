e9bdabd27e0598af427a90e18c1a2ab92aa5db87
zhangzongzheng <zhangzongzheng@bilibili.com>
2017-12-26 19:07:59 +0800

    first commit

---
+++ examples/index.js
@@ -0,0 +1,6 @@
+const express = require('../index')
+// const express = require('express')
+const app = express()
+app.listen(3000, function (req, res) {
+  console.log('server is listening 3000')
+})
