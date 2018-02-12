72e2604ebc903a43dd873f5c9ead88f590180070
zhangzongzheng <zhangzongzheng@bilibili.com>
2018-02-12 19:01:38 +0800

    create server

--- examples/index.js
+++ examples/index.js
@@ -1,6 +1,20 @@
 const express = require('../index')
 // const express = require('express')
 const app = express()
+
+// app.get('/', function(req, res) {
+//   res.end('You send GET request')
+// })
+
+// app.post('/', function(req, res) {
+//   res.end('You send POST request')
+// })
+
+// app.use((req, res, next) => {
+//   console.log('this is middleware1')
+//   next()
+// })
+
 app.listen(3000, function (req, res) {
   console.log('server is listening 3000')
 })
