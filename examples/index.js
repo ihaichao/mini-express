cf6f4745ecf53978adeb862d30e1b5ec1d4c0966
zhangzongzheng <zzzhaichao@gmail.com>
2018-02-19 22:55:19 +0800

    implement router

--- examples/index.js
+++ examples/index.js
@@ -2,13 +2,9 @@ const express = require('../index')
 // const express = require('express')
 const app = express()
 
-// app.get('/', function(req, res) {
-//   res.end('You send GET request')
-// })
-
-// app.post('/', function(req, res) {
-//   res.end('You send POST request')
-// })
+app.get('/', function(req, res) {
+  res.end('hello express')
+})
 
 // app.use((req, res, next) => {
 //   console.log('this is middleware1')
@@ -16,5 +12,5 @@ const app = express()
 // })
 
 app.listen(3000, function (req, res) {
-  console.log('server is listening 3000')
+  console.log('server is listening port 3000')
 })
