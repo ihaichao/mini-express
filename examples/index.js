db97403146374fd55b7d40cf514fa171e6b6c17c
zhangzongzheng <zhangzongzheng@bilibili.com>
2018-02-22 18:51:34 +0800

    implement middleware

--- examples/index.js
+++ examples/index.js
@@ -6,10 +6,15 @@ app.get('/', function(req, res) {
   res.end('hello express')
 })
 
-// app.use((req, res, next) => {
-//   console.log('this is middleware1')
-//   next()
-// })
+app.use((req, res, next) => {
+  console.log('this is middleware1')
+  next()
+})
+
+app.use((req, res, next) => {
+  console.log('this is middleware2')
+  next()
+})
 
 app.listen(3000, function (req, res) {
   console.log('server is listening port 3000')
